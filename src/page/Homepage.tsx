31104085cea1f2481f4b9a2c0707360bbc63277e
Onebluesky882 <onebluesky882@outlook.com>
2025-04-23 19:33:10 +0700

    add button homepage

--- src/page/Homepage.tsx
+++ src/page/Homepage.tsx
@@ -1,4 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 const Homepage = () => {
-  return <div>h1 Homepage</div>;
+  return (
+    <div>
+      <h1>​Zap logistic</h1>
+      <a href="/product-table">
+        <Button>Products Table</Button>
+      </a>
+    </div>
+  );
 };
 export default Homepage;
