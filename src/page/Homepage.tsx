1323a542bf36ffd26a3d95f6ba438555ac54d173
Onebluesky882 <onebluesky882@outlook.com>
2025-04-23 19:34:01 +0700

    css homepage

--- src/page/Homepage.tsx
+++ src/page/Homepage.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 const Homepage = () => {
   return (
-    <div>
+    <div className="flex-1 justify-center">
       <h1>​Zap logistic</h1>
       <a href="/product-table">
         <Button>Products Table</Button>
